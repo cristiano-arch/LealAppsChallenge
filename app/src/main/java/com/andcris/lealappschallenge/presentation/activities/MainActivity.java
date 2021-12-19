ed20dc1847bce4d7a3e4e051e024d6379c420caa
Cristiano Jr <cristianoandre96@gmail.com>
2021-12-19 18:34:38 -0200

    Connecting to Firebase
    Firestore working

--- app/src/main/java/com/andcris/lealappschallenge/presentation/activities/MainActivity.java
+++ app/src/main/java/com/andcris/lealappschallenge/presentation/activities/MainActivity.java
@@ -1,13 +1,25 @@
 package com.andcris.lealappschallenge.presentation.activities;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-
 import com.andcris.lealappschallenge.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +27,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(activityMainBinding.getRoot());
 
-        startActivity(new Intent(MainActivity.this, HomeActivity.class));
-        finish();
+        testDbConnection();
+
+        /*startActivity(new Intent(MainActivity.this, HomeActivity.class));
+        finish();*/
+    }
+
+    private void testDbConnection() {
+        db.collection("workouts")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (!task.getResult().isEmpty()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                Toast.makeText(MainActivity.this, "Yeah!", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                            Toast.makeText(MainActivity.this, "Oops!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
