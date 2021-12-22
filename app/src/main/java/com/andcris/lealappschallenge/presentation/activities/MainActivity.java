b2734089c719fea6db9def891137cc2ba1476c24
Cristiano Jr <cristianoandre96@gmail.com>
2021-12-22 14:55:22 -0200

    Verifying if user is already connected

--- app/src/main/java/com/andcris/lealappschallenge/presentation/activities/MainActivity.java
+++ app/src/main/java/com/andcris/lealappschallenge/presentation/activities/MainActivity.java
@@ -6,8 +6,12 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.andcris.lealappschallenge.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(activityMainBinding.getRoot());
 
-        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        } else {
+            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+        }
         finish();
     }
 }
